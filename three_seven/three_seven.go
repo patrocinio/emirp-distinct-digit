1229650a4c3ac6adba03116518cf431dc782104c
Eduardo Patrocinio <epatro@gmail.com>
2021-12-22 15:23:08 -0300

    three or seve

--- three_seven/three_seven.go
+++ three_seven/three_seven.go
@@ -5,9 +5,9 @@ import (
 	"math"
 )
 
-func prime_number(num int) bool {
-	sq_root := int(math.Sqrt(float64(num)))
-	for i := 2; i <= sq_root; i++ {
+func prime_number(num uint64) bool {
+	sq_root := uint64(math.Sqrt(float64(num)))
+	for i := uint64(2); i <= sq_root; i++ {
 		if num%i == 0 {
 			return false
 		}
@@ -15,21 +15,24 @@ func prime_number(num int) bool {
 	return true
 }
 
-func add(num int, digit int) {
+func add(num uint64, digit uint64) {
 	var i = num*10 + digit
-	fmt.Printf("add i: %d\n", i)
+	if i%10 != 3 && i%10 != 7 {
+		return
+	}
+	//	fmt.Printf("add i: %d\n", i)
 	if prime_number(i) {
 		fmt.Printf("==> i: %d\n", i)
 	}
 	run(i)
 }
 
-func run(num int) {
-	fmt.Printf("run num: %d\n", num)
-	//	add(num, 3)
-	//	add(num, 7)
+func run(num uint64) {
+	//	fmt.Printf("run num: %d\n", num)
+	add(num, 3)
+	add(num, 7)
 }
 
 func main() {
-	//	run(0)
+	run(0)
 }
